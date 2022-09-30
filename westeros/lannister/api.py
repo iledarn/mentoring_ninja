5e3b457e6fba10081bac2074f48864fb2c13e81d
iledarn <iledarn@gmail.com>
2022-09-30 13:24:27 +0800

    add - targaryen app

--- westeros/lannister/api.py
+++ westeros/lannister/api.py
@@ -8,6 +8,7 @@ router = Router()
 def home(request):
     return "A Lannister always pays their debts"
 
+
 @router.get("/rock/{int:height}")
-def rock(request, height:int):
+def rock(request, height: int):
     return f"Casterly Rock is {height + 1}m tall"
