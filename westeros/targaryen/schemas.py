623030c6e1c78d1b8fdec4e5f2bc3c212af59839
iledarn <iledarn@gmail.com>
2022-09-30 15:39:59 +0800

    add - ModelSchema for Person

--- westeros/targaryen/schemas.py
+++ westeros/targaryen/schemas.py
@@ -1,6 +1,12 @@
-from ninja import Schema
+from ninja import Schema, ModelSchema
+from targaryen.models import Person
 
 
 class DragonOut(Schema):
     name: str
     birth_year: int
+
+
+class PersonOut(ModelSchema):
+    class Config:
+        model = Person
