8cf1319d63f83faeaaede0cea1dca3d86ba699d3
iledarn <iledarn@gmail.com>
2022-09-30 15:14:54 +0800

    add - Person model

--- westeros/targaryen/models.py
+++ westeros/targaryen/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    birth_year = models.PositiveSmallIntegerField()
