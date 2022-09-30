41b397dcf42b1e6bf1cdba8a77e67140eef6965b
iledarn <iledarn@gmail.com>
2022-09-30 11:42:03 +0800

    add - rock interface

--- westeros/lannister/api.py
+++ westeros/lannister/api.py
@@ -7,3 +7,7 @@ router = Router()
 @router.get("/home")
 def home(request):
     return "A Lannister always pays their debts"
+
+@router.get("/rock")
+def rock(request, height):
+    return f"Casterly Rock is {height}m tall"
